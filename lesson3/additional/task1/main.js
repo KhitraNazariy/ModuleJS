1313e37ac15daffab6dd729799d66550aae5d507
KhitraNazariy <khitranazariy19@gmail.com>
2021-11-17 22:46:45 +0200

    lesson 3

--- lesson3/additional/task1/main.js
+++ lesson3/additional/task1/main.js
@@ -99,6 +99,53 @@
 // }
 // console.log(newArray);
 
+
+// - Дано 2 масиви з рівною кількістю об'єктів.
+// Масиви:
+//
+// З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+//     Записати цей об'єкт в новий масив
+// Example:
+//     let usersWithCities = [
+//         {
+//             id: 1, // <===
+//             name: 'vasya',
+//             age: 31,
+//             status: false,
+//             address: {
+//                 user_id: 1, // <===
+//                 country: 'Ukraine',
+//                 city: 'Ternopil'
+//             }
+//         },
+//         // TO BE CONTINUED .....
+//     ]
+
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+//
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+//
+// for (let i = 0; i < usersWithId.length; i++) {
+//     let user = usersWithId[i];
+//     for (const city of citiesWithId) {
+//         if (city.user_id === user.id) {
+//             user.city = city;
+//         }
+//     }
+// }
+// console.log(usersWithId);
+
+
 // - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
 
 // let array = [644, 93, 64, 551, 728, 249, 294, 72, 422, 457];
